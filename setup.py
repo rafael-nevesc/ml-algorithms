00182a516c42040e2ab01912ed036e39511bf5eb
rockzecron <rafael.neves@graduacao.ie.ufrj.br>
2023-09-13 21:26:50 -0300

    Init commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project builds popular machine learning algorithms from scratch using numpy, inspired by AssemblyAI video series in YouTube.',
+    author='Rafael Neves',
+    license='MIT',
+)
